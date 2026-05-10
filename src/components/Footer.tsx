d77c0f1ca2e33dff3860cda75bc1992ebe7d5691
code-leaf <c0mwgave1407@gmail.com>
2026-05-10 18:39:17 +0900

    ♻️ 著作権年を動的に更新（毎年自動）

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className='bg-white shadow-md'>
       <div className='container mx-auto px-6 py-4'>
         <p className='text-center text-gray-600'>
-          © 2024 CodeLeaf. All Rights Reserved.
+          © {new Date().getFullYear()} CodeLeaf. All Rights Reserved.
         </p>
       </div>
     </footer>
